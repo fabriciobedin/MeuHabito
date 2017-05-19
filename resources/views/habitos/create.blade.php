b92798bc8aa8869ec0e16fdbe5a0d18d8cefdb4d
Fabricio Bedin <fabricioobedin@gmail.com>
2017-05-18 22:16:18 -0300

    add create - final aula 2017-05-18

--- resources/views/habitos/create.blade.php
+++ resources/views/habitos/create.blade.php
@@ -3,5 +3,17 @@
 @section('content')
     <div class="container">
         <h1>Novo Hábito</h1>
+
+        {!! Form::open() !!}
+        <div class="form-group">
+            {!! Form::label('nome', 'Nome') !!}
+            {!! Form::text('nome', null,['class' =>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('descricao', 'Descrição') !!}
+            {!! Form::textarea('descricao', null,['class' =>'form-control']) !!}
+        </div>
+        {!! Form::close() !!}
     </div>
 @endsection
